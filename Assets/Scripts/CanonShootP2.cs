de03680b6345a57102d3df9598a75c8434990791
BaberinosLeDouxTacos <39302412+BaberinosLeDouxTacos@users.noreply.github.com>
2018-10-01 22:36:01 +0200

    Zeubi , j'ai oublié le push ( Split Screen + Shoot )

---
+++ Assets/Scripts/CanonShootP2.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanonShootP2 : MonoBehaviour 
+{
+		private GameObject CannonBall;
+		public GameObject CanonHead;
+		public GameObject BallPrefab;
+		public Transform BallSpawn;
+		public float SpawnRadius = 2f;
+		public float BallSpeed = 40f;
+
+		private void Update() 
+		{
+			if(Input.GetButtonDown("FireP2"))
+			{
+				Fire();
+			}
+		}
+
+		void Fire()
+		{
+			CannonBall = Instantiate(BallPrefab, BallSpawn.position, BallSpawn.rotation);
+			CannonBall.GetComponent<Rigidbody>().velocity = CannonBall.transform.forward*BallSpeed;
+
+		}
+
+		private void OnDrawGizmos() 
+		{
+			Gizmos.color = Color.magenta;
+			Gizmos.DrawWireSphere(BallSpawn.position,SpawnRadius);
+		}
+}
---
+++ Assets/Scripts/ProjectilBehavior.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectilBehavior : MonoBehaviour 
+{
+	private void OnCollisionEnter(Collision other) 
+		{
+			Destroy(gameObject);
+		}
+
+}
