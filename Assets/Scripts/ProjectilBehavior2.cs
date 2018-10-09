1c3e3a07308279007d7ed2ae6e7ec5e994464662
BaberinosLeDouxTacos <39302412+BaberinosLeDouxTacos@users.noreply.github.com>
2018-10-09 09:14:40 +0200

    FX + HP

--- Assets/Scripts/ProjectilBehavior2.cs
+++ Assets/Scripts/ProjectilBehavior2.cs
@@ -3,15 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ProjectilBehavior2 : MonoBehaviour 
-
 {
-	
+	public GameObject[] ParticlesToSpawn;
+	public float BlastRadius = 5f;
+
+
 
 	private void OnCollisionEnter(Collision other) 
 		{
-			GameObject.Find("Player2_Kart").GetComponent<CanonShootP2>().isDestroyed=true;
-			Destroy(gameObject);
-	
-		}
+			
+			if (other.transform.tag =="WaterSplash") 
+			{
+				GameObject.Find("Player1_Kart").GetComponent<CanonShootP1>().isDestroyed=true;
+				Instantiate(ParticlesToSpawn[4], transform.position, transform.rotation);
+				Destroy(gameObject);
+			}
+			else if (other.transform.tag =="TerrainTag")
+			{
+				GameObject.Find("Player1_Kart").GetComponent<CanonShootP1>().isDestroyed=true;
+				Instantiate(ParticlesToSpawn[0], transform.position, transform.rotation);
+				Destroy(gameObject);
+			}
+			else if (other.transform.tag =="PlayerTag")
+			{
+				GameObject.Find("Player1_Kart").GetComponent<CanonShootP1>().isDestroyed=true;
+				Instantiate(ParticlesToSpawn[1], transform.position, transform.rotation);
+				Destroy(gameObject);
+			}
+			else if (other.transform.tag == "TreeTag")
+			{
+				GameObject.Find("Player1_Kart").GetComponent<CanonShootP1>().isDestroyed=true;
+				Instantiate(ParticlesToSpawn[2], transform.position, transform.rotation);
+				Destroy(gameObject);
+			}
+			else if (other.transform.tag == "RockTag")
+			{
 
+				GameObject.Find("Player1_Kart").GetComponent<CanonShootP1>().isDestroyed=true;
+				Instantiate(ParticlesToSpawn[3], transform.position, transform.rotation);
+				Destroy(gameObject);
+			}else{
+				print(other.transform.name);
+			}
+	
+		}	
+		
 }
---
+++ Assets/Scripts/StateP1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StateP1 : MonoBehaviour 
+{
+	public float HealthPoint = 100;
+	private float Health;
+	public GameObject DeathEffect;
+	public Image HealtBar;
+
+
+	void Start()
+	{
+		Health = HealthPoint;
+	}
+
+	public void Damaged ( float Amount)
+	{
+		HealthPoint -= Amount;
+		HealtBar.fillAmount =  Health / HealthPoint;
+		
+	}
+}
